237ea23b3d3b59ddde44eda93ef89051888e8857
mariefj <marie.forja@gmail.com>
2023-01-05 14:54:52 +0100

    fix(): booking only in future competitions

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import json
+from datetime import datetime
+
 from flask import Flask,render_template,request,redirect,flash,url_for
+
 
 MAX_PLACES = 12
 
@@ -18,8 +21,10 @@ def loadCompetitions():
 app = Flask(__name__)
 app.secret_key = 'something_special'
 
+
 competitions = loadCompetitions()
 clubs = loadClubs()
+current_date = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
 
 cart = {
     competition['name']: {club['name']: 0 for club in clubs}
@@ -45,8 +50,10 @@ def showSummary():
 def book(competition,club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
+
     if foundClub and foundCompetition:
         return render_template('booking.html',club=foundClub,competition=foundCompetition)
+
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
@@ -59,12 +66,18 @@ def purchasePlaces():
     placesRequired = int(request.form['places'])
     current_cart = cart[competition['name']][club['name']]
 
-    if placesRequired > MAX_PLACES :
+    if competition['date'] < current_date:
+        flash("Sorry, this competition is over")
+        return render_template('welcome.html', club=club, competitions=competitions)  
+
+    elif placesRequired > MAX_PLACES :
         flash(f"Sorry, you can't book more than {MAX_PLACES} places")
         return render_template('booking.html', club=club, competition=competition)
+
     elif placesRequired > clubPoints:
         flash("Sorry, your club doesn't have enough points")
         return render_template('booking.html', club=club, competition=competition)
+
     elif placesRequired + current_cart > MAX_PLACES:
         flash(f"""Sorry, you have already booked {current_cart} places for this competition 
         and you can't book more than {MAX_PLACES}""")
@@ -73,6 +86,7 @@ def purchasePlaces():
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     club['points'] = int(club['points'])- placesRequired
     cart[competition["name"]][club["name"]] += placesRequired
+
     flash(f"Great you have booked {placesRequired} places for {competition['name']}")
     return render_template('welcome.html', club=club, competitions=competitions)
 
